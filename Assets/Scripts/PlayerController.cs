ef7277db13e5c6679234bbc129cbf11b35b7aead
Elena Tabakova <elena.tabakova@nexnet.de>
2020-11-13 18:17:13 +0100

    Revert "fix"
    This reverts commit 1037754b79629ffd0c2de4e4f34f91b1db9d7ae2.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,67 +1,39 @@
 using UnityEngine;
 using System.Collections;
-using System.Threading;
-using System.Diagnostics;
 
-namespace Completed
-{
-    public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D _rigidBody;
+
+	private float horizontal;
+	private float vertical;
+    void Start()
     {
-
-        public GameManager gm;
-        public float speed;
-        private Rigidbody2D _rigidBody;
-
-        private float horizontal;
-        private float vertical;
-
-        void Start()
-        {
-            gm = GameObject.Find("GameManager").GetComponent<GameManager>();
-            _rigidBody = GetComponent<Rigidbody2D>();
-            Vector3 pos = transform.position;
-            UnityEngine.Debug.Log("player: " + pos);
-        }
-
-        void Update()
-        {
-            horizontal = Input.GetAxisRaw("Horizontal");
-            vertical = Input.GetAxisRaw("Vertical");
-        }
-
-        private void FixedUpdate()
-        {
-            // if( horizontal != 0 && vertical !=0) // check for diagonal movement?
-            // velocity is both the speed and the direction 
-            _rigidBody.velocity = new Vector2(horizontal * speed, vertical * speed);
-        }
-
-        void OnCollisionEnter2D(Collision2D col)
-        {
-            if (col.gameObject.name == "fire-03")
-            {
-                Object.Destroy(col.gameObject);
-
-            }
-            else if (col.gameObject.CompareTag("Enemy"))
-            {
-
-                StartCoroutine(Attacked());
-                gm.Lifes--;
-                if (gm.Lifes <= 0)
-                {
-                    //!!!!I tried to switch the state from the GameManager but still havent found any solution so I'm just gonna load the panelGameOver right now 
-                    gm.panelGameOver.SetActive(true);
-                }
-            }
-        }
-        IEnumerator Attacked()
-        {
-
-            GetComponent<SpriteRenderer>().color = Color.red;
-            yield return new WaitForSeconds(0.5f);
-            GetComponent<SpriteRenderer>().color = Color.white;
-
-        }
+    	_rigidBody = GetComponent<Rigidbody2D>();
+    	Vector3 pos = transform.position;
+    	Debug.Log("player: " + pos); 
     }
+	
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+    
+    private void FixedUpdate()
+    {
+    	// if( horizontal != 0 && vertical !=0) // check for diagonal movement?
+    	// velocity is both the speed and the direction 
+        _rigidBody.velocity = new Vector2 (horizontal * speed, vertical * speed);
+    }
+    
+    void OnCollisionEnter2D(Collision2D col)
+     {
+         if (col.gameObject.name == "fire-03")
+         {
+             Object.Destroy(col.gameObject);
+             
+         }
+     }
 }
